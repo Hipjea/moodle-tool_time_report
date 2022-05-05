4c7164158cb29bff1889a900d5cf217edba9580e
Hipjea <pierre.duverneix@gmail.com>
2022-05-05 10:38:32 +0200

    add userid to fileinfo

--- locallib.php
+++ locallib.php
@@ -12,6 +12,29 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
+ * Retrives the files of existing reports
+ *
+ * @return Array of moodle_url
+ */
+function get_reports_urls($contextid, $userid) {
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($contextid, 'tool_time_report', 'content');
+    $out = array();
+
+    foreach ($files as $file) {
+        $filename = $file->get_filename();
+        $file_userid = get_user_id_from_filename($filename);
+        if ($file_userid == $userid && $filename != '.') {
+            $path = '/' . $file->get_contextid() . '/tool_time_report/content/' . $file->get_itemid() . $file->get_filepath() . $filename;
+            $url = moodle_url::make_file_url('/pluginfile.php', $path);
+            array_push($out, array('url' => $url, 'filename' => $filename));
+        }
+    }
+
+    return $files
+}
+
+/**
  * Retrives the moodle_url of existing reports
  *
  * @return Array of moodle_url
@@ -60,4 +83,27 @@ function get_user_id_from_filename($filename) {
         return intval($parts[2]);
     }
     return false;
+}
+
+/**
+ * Removes the report files for a given user.
+ *
+ * @return Array of moodle_url
+ */
+function get_reports_urls($contextid, $userid) {
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($contextid, 'tool_time_report', 'content');
+    $out = array();
+
+    foreach ($files as $file) {
+        $filename = $file->get_filename();
+        $file_userid = get_user_id_from_filename($filename);
+        if ($file_userid == $userid && $filename != '.') {
+            $path = '/' . $file->get_contextid() . '/tool_time_report/content/' . $file->get_itemid() . $file->get_filepath() . $filename;
+            $url = moodle_url::make_file_url('/pluginfile.php', $path);
+            array_push($out, array('url' => $url, 'filename' => $filename));
+        }
+    }
+
+    return $out;
 }
